9f081ee1b3b0bd5cc2f4ddd7a0e02a45ca95983e
Seksan <microsekmcu@gmail.com>
2016-10-21 13:35:39 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,3 @@
 <?php
-$ curl -XGET -u "$CLOUDMQTT_USER:$CLOUDMQTT_PASSWORD" https://api.cloudmqtt.com/user
-#=> [{"username":"test","password":"$6$b1NBWGlJR1dEVEg5$8LzJAblro5HL7dj8ro7RLRKOD5X6BqlqvXZQb9kcuZkDbWHi0oACO1QF2OTLGFrUnXAqbtd538jf9fQZm+BtLw=="}]
+$ curl -XGET -u "microsekmcu@gmail.com:pass2526" https://api.cloudmqtt.com/acl
+[{"topic":"test/users/topic","read":true,"write":true,"username":"topic"}]
