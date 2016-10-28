e23b7195f03103cf78be17db953b209a956fd9dd
Seksan <microsekmcu@gmail.com>
2016-10-28 09:24:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,13 +18,40 @@ if (!is_null($events['events'])) {
 			}
 			elseif($text_in=='เปิด')
 			{
-				echo  $_POST['https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1'];
-				$text = "เปิด1แล้วครับเจ้านาย";
+				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/2920959.json?api_key=WFN45I92A9NN3S27';
+				$data = array('command_string' => 'ON', 'position' => '1');
+
+				$options = array(
+    					'http' => array(
+        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        				'method'  => 'PUT',
+        				'content' => http_build_query($data)
+    						)
+				);
+				$context  = stream_context_create($options);
+				$result = file_get_contents($url, false, $context);
+				if ($result === FALSE) { /* Handle error */ }
+					var_dump($result);
+				$text = "เปิดแล้วครับเจ้านาย";
 			}
 			elseif($text_in=='ปิด')
 			{
-				echo  $_POST['https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=off&position=1'];
-				$text = "ปิด1แล้วครับเจ้านาย";
+				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/2920959.json?api_key=WFN45I92A9NN3S27';
+				$data = array('command_string' => 'OFF', 'position' => '1');
+
+				$options = array(
+    					'http' => array(
+        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        				'method'  => 'PUT',
+        				'content' => http_build_query($data)
+    						)
+				);
+				$context  = stream_context_create($options);
+				$result = file_get_contents($url, false, $context);
+				if ($result === FALSE) { /* Handle error */ }
+					var_dump($result);
+				$text = "ปิดแล้วครับเจ้านาย";
+				
 			}
 			else
 			{
