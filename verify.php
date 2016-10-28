d707c0ebe6fce49fade93fcb179830f94f3e0ae5
Seksan <microsekmcu@gmail.com>
2016-10-28 09:19:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,11 @@
 <?php
-$url = 'https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27';
-$data = array('command_string' => 'on', 'position' => '1');
+$url = 'https://api.thingspeak.com/talkbacks/10962/commands/2920959.json?api_key=WFN45I92A9NN3S27';
+$data = array('command_string' => '123', 'position' => '1');
 // use key 'http' even if you send the request to https://...
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
+        'method'  => 'PUT',
         'content' => http_build_query($data)
     )
 );
@@ -13,5 +13,4 @@ $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 if ($result === FALSE) { /* Handle error */ }
 var_dump($result);
-echo "finish";
 ?>
