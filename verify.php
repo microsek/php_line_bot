e7977a716584f15538300e8b4fdd24745a09a82e
Seksan <microsekmcu@gmail.com>
2016-10-28 09:00:15 +0700

    update curl

--- verify.php
+++ verify.php
@@ -1,5 +1,17 @@
 <?php
-$response = http_post_fields("https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1");
-echo "OK";
+$url = 'https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27';
+$data = array('command_string' => 'on', 'position' => '1');
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+var_dump($result);
+echo "finish";
 ?>
-
