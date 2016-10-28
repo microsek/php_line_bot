b21c9fe972b4970c5161e6f1135f29b54bd41e9b
Seksan <microsekmcu@gmail.com>
2016-10-28 08:54:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,5 @@
 <?php
-echo $_POST["https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1"]."<br>"; // txtName
+$response = http_post_fields("https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1");
 echo "OK";
 ?>
+
