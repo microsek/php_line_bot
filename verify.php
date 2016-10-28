74e5f6cafbe6e7535a0d2c66532eb144c931397b
Seksan <microsekmcu@gmail.com>
2016-10-28 08:42:00 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,4 @@
-<html>
-<head>
-<title>ThaiCreate.Com Tutorial</title>
-</head>
-<body>
 <?php
-echo $_REQUEST["https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1"]."<br>"; // txtName
+echo $_POST["https://api.thingspeak.com/talkbacks/10962/commands?api_key=WFN45I92A9NN3S27&command_string=on&position=1"]."<br>"; // txtName
 echo "OK";
 ?>
-</body>
-</html>
