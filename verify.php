d6ab5cf652ab924dd2d18e8b8ca2f24cfeacbe52
Seksan <microsekmcu@gmail.com>
2016-10-31 17:44:24 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,2 +1,17 @@
 <?php
-echo $_GET["https://api.thingspeak.com/talkbacks/10962/commands.json?api_key=WFN45I92A9NN3S27"];
+
+$url = 'https://api.thingspeak.com/talkbacks/10962/commands.json?';
+				$data = array('api_key' => 'WFN45I92A9NN3S27');
+				$options = array(
+    					'http' => array(
+        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        				'method'  => 'GET',
+        				'content' => http_build_query($data)
+    						)
+				);
+				$context  = stream_context_create($options);
+				$result = file_get_contents($url, false, $context);
+				if ($result === FALSE) { /* Handle error */ }
+					var_dump($result);
+
+echo $result;
