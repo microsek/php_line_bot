ea632ba0ac346d7dbdaca3624c26d386e9b9f6e8
Seksan <microsekmcu@gmail.com>
2016-10-31 18:11:52 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,29 +1,20 @@
 <?php
-
-$url = 'https://api.thingspeak.com/talkbacks/10962/commands.json?';
-				$data = array('api_key' => 'WFN45I92A9NN3S27');
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'GET',
-        				'content' => http_build_query($data)
-    						)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
-
-$content = file_get_contents('php://input');
+// Get POST body content
+$content = file_get_contents('https://api.thingspeak.com/talkbacks/10962/commands.json?api_key=WFN45I92A9NN3S27');
+// Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			$text_in = $event['message']['text'];
+		if ($event['type'] == 'command_string' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text_in = $event['command_string']['text'];
+			// Get replyToken
+			
+			echo $text_in . "\r\n";
 		}
 	}
 }
-echo $text_in;
+echo "OK";
