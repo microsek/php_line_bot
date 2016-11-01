ece50728e1201ff0c29464130525362302771b16
Seksan <microsekmcu@gmail.com>
2016-11-01 08:15:34 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,8 @@
 <?php
-// Get POST body content
 $content = file_get_contents('https://api.thingspeak.com/talkbacks/10962/commands.json?api_key=WFN45I92A9NN3S27');
-// Parse JSON
 $events = json_decode($content, true);
-print $events;//->{'command_string'};
 foreach ($events as $result) {
    echo $result["command_string"];
+   echo "<br>";
 }
 
