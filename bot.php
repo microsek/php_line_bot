ec5326d76d7956bb29c23234f50e8ceaa73c5aae
Seksan <microsekmcu@gmail.com>
2016-11-01 08:30:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,7 +58,7 @@ if (!is_null($events['events'])) {
 				$content = file_get_contents('https://api.thingspeak.com/talkbacks/10962/commands.json?api_key=WFN45I92A9NN3S27');
 				$events = json_decode($content, true);
 				foreach ($events as $result) {
-   					$status=$result["command_string"].",";
+   					$status=$result["command_string"].",".$status;
 				}
 				$text = $status;
 			}
