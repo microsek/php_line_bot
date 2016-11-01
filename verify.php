27229d5ef86967be6454a9a0a5589b8f8ea0bcbc
Seksan <microsekmcu@gmail.com>
2016-11-01 08:21:29 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $content = file_get_contents('https://api.thingspeak.com/talkbacks/10962/commands.json?api_key=WFN45I92A9NN3S27');
 $events = json_decode($content, true);
 foreach ($events as $result) {
-   echo $result["command_string"];
-   echo "<br>";
+   $test=$result["command_string"];
+   echo $test;
 }
 
