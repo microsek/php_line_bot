72480b58d361f04cdb55fa3ad9e5dc5c228014e1
Seksan <microsekmcu@gmail.com>
2016-12-06 15:14:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -110,10 +110,11 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
-				//"type": "text",
-                                //"text": "Hello, world"
-				'type' => 'text',
-				'text' => "Hello, world"
+				//'type' => 'text',
+				//text' => "Hello, world"
+				 'type'=> 'sticker',
+ 				 'packageId'=> "1",
+				 'stickerId'=> "1"
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
