a3d3032025ef6deac1157e91e4008f030e1891da
Seksan <microsekmcu@gmail.com>
2017-04-06 15:26:34 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,18 +1,20 @@
 <?php
     $node=$_GET=["node"];
     $cmd=$_GET=["cmd"];
+    echo $node;
+    echo $cmd;
     $FIREBASE = "https://esp8266-temp.firebaseio.com/";
-				$NODE_PATCH = "Lamp.json";
-				$data = array(
-    					$node => $cmd
-				);
-				$json = json_encode( $data );
-				$curl = curl_init();
-				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
-				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
-				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
-				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-				$response = curl_exec( $curl );
-				curl_close( $curl );
-				echo $response . "\n";
+	$NODE_PATCH = "Lamp.json";
+	$data = array(
+    		$node => $cmd
+	);
+	$json = json_encode( $data );
+	$curl = curl_init();
+	curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+	curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+	curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	$response = curl_exec( $curl );
+	curl_close( $curl );
+	echo $response . "\n";
 ?>
