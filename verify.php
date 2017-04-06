4f8d113ecf8b89398285d91b11ad56b3666243cd
Seksan <microsekmcu@gmail.com>
2017-04-06 12:52:56 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,16 +1,42 @@
 
 <?php
-
-$data = array("LED1" => 55);
-        $ch = curl_init("https://esp8266-temp.firebaseio.com/Lamp.json");
- 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
- 
-        $response = curl_exec($ch);
-        if(!$response) {
-            return false;
-        }
-
-echo "OK";
+// Constants
+$FIREBASE = "https://esp8266-temp.firebaseio.com/Lamp.json";
+$NODE_DELETE = "temperature.json";
+$NODE_GET = "temperature.json";
+$NODE_PATCH = ".json";
+$NODE_PUT = "temperature.json";
+// Data for PUT
+// Node replaced
+$data = 32;
+// Data for PATCH
+// Matching nodes updated
+$data = array(
+    "LED1" => 42
+);
+// JSON encoded
+$json = json_encode( $data );
+// Initialize cURL
+$curl = curl_init();
+// Create
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PUT );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PUT" );
+// curl_setopt( $curl, CURLOPT_POSTFIELDS, 32 );
+// Read
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_GET );
+// Update
+curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+// Delete
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_DELETE );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "DELETE" );
+// Get return value
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+// Make request
+// Close connection
+$response = curl_exec( $curl );
+curl_close( $curl );
+// Show result
+echo $response . "\n";
+?>
