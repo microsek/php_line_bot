1bfce140769894a5b6df5831c121fea3aac67dd6
Seksan <microsekmcu@gmail.com>
2017-04-06 15:38:49 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -4,7 +4,7 @@
     $FIREBASE = "https://esp8266-temp.firebaseio.com/";
 	$NODE_PATCH = "Lamp.json";
 	$data = array(
-    		$_POST=['node'] => $_POST=['cmd']
+    		$_POST=["node"] => $_POST=["cmd"]
 	);
 	$json = json_encode( $data );
 	$curl = curl_init();
