b5a82f4220b1d0ee8c564644c9ed9708aa363ae4
Seksan <microsekmcu@gmail.com>
2017-04-06 14:09:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,110 +23,78 @@ if (!is_null($events['events'])) {
 			}
 			elseif($text_in=='เปิดLED1')
 			{
-				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/4117441.json?api_key=WFN45I92A9NN3S27';
-				$data = array('command_string' => 'LED1=ON', 'position' => '2');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
+				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
+				$NODE_PATCH = "Lamp.json";
+				$data = array(
+    					"LED1" => 1
 				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
+
+				$json = json_encode( $data );
+				$curl = curl_init();
+				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+				$response = curl_exec( $curl );
+				curl_close( $curl );
+				echo $response . "\n";
 				$messages = "เปิดLED1แล้วครับเจ้านาย";
-			}
-			elseif($text_in=='เปิด')
-			{
-				$url = 'https://esp8266-temp.firebaseio.com/LED.json';
-				$data = array('LED' => '1');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
-				$messages = "update Firebase แล้วครับ";
-			}elseif($text_in=='ปิด')
-			{
-				$url = 'https://esp8266-temp.firebaseio.com/LED.json';
-				$data = array('LED' => '0');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
-				$messages = "update ปิด Firebase แล้วครับ";
-			}
-			elseif($text_in=='ปิดLED1')
-			{
-				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/4117441.json?api_key=WFN45I92A9NN3S27';
-				$data = array('command_string' => 'LED1=OFF', 'position' => '2');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
-				$messages = "ปิดLED1แล้วครับเจ้านาย";
-				
 			}
 			elseif($text_in=='เปิดLED2')
 			{
-				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/4117582.json?api_key=WFN45I92A9NN3S27';
-				$data = array('command_string' => 'LED2=ON', 'position' => '1');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
+				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
+				$NODE_PATCH = "Lamp.json";
+				$data = array(
+    					"LED2" => 1
 				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
+
+				$json = json_encode( $data );
+				$curl = curl_init();
+				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+				$response = curl_exec( $curl );
+				curl_close( $curl );
+				echo $response . "\n";
 				$messages = "เปิดLED2แล้วครับเจ้านาย";
+			}elseif($text_in=='ปิดLED1')
+			{
+				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
+				$NODE_PATCH = "Lamp.json";
+				$data = array(
+    					"LED1" => 0
+				);
+
+				$json = json_encode( $data );
+				$curl = curl_init();
+				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+				$response = curl_exec( $curl );
+				curl_close( $curl );
+				echo $response . "\n";
+				$messages = "ปิดLED1แล้วครับเจ้านาย";
+
 			}
 			elseif($text_in=='ปิดLED2')
 			{
-				$url = 'https://api.thingspeak.com/talkbacks/10962/commands/4117582.json?api_key=WFN45I92A9NN3S27';
-				$data = array('command_string' => 'LED2=OFF', 'position' => '1');
-
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
+				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
+				$NODE_PATCH = "Lamp.json";
+				$data = array(
+    					"LED2" => 0
 				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
+
+				$json = json_encode( $data );
+				$curl = curl_init();
+				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+				$response = curl_exec( $curl );
+				curl_close( $curl );
+				echo $response . "\n";
 				$messages = "ปิดLED2แล้วครับเจ้านาย";
 				
 			}
