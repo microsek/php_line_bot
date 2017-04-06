7cb3daefbdb1f887707058cdb9a5ce92eeafbc75
Seksan <microsekmcu@gmail.com>
2017-04-06 11:43:11 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,16 +1,15 @@
 
 <?php
 $url = 'https://esp8266-temp.firebaseio.com/LED.json';
-				$data = array("LED" => 55);
-				$options = array(
-    					'http' => array(
-        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  => 'PUT',
-        				'content' => http_build_query($data)
-    						)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				if ($result === FALSE) { /* Handle error */ }
-					var_dump($result);
-
+$data = array("LED" => 55);
+$options = array(
+	'http' => array(
+		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		'method'  => 'PUT',
+		'content' => http_build_query($data))
+		);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+	var_dump($result);
+echo "OK";
