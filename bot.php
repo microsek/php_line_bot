141713200b669300e916842ac3122786200669be
Seksan <microsekmcu@gmail.com>
2017-04-11 21:56:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,12 +28,12 @@ if (!is_null($events['events'])) {
 				 'stickerId'=> "1"				
 			        ];	
 			}
-			elseif($textin_cmd[0]=='เปิด')
+			elseif($textin_cmd[0]=='update')
 			{
 				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
 				$NODE_PATCH = "Lamp.json";
 				$data = array(
-    					$textin_cmd[1] => 1
+    					$textin_cmd[1] => $textin_cmd[2]
 				);
 
 				$json = json_encode( $data );
@@ -50,28 +50,7 @@ if (!is_null($events['events'])) {
 					'text' => "เรียบร้อยแล้วครับเจ้านาย"
 				];
 			}
-			elseif($textin_cmd[0]=='ปิด')
-			{
-				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
-				$NODE_PATCH = "Lamp.json";
-				$data = array(
-    					$textin_cmd[1] => 0
-				);
-
-				$json = json_encode( $data );
-				$curl = curl_init();
-				curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
-				curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
-				curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
-				curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-				$response = curl_exec( $curl );
-				curl_close( $curl );
-				echo $response . "\n";
-				$messages = [
-					'type' => 'text',
-					'text' => "เรียบร้อยแล้วครับเจ้านาย"
-				];
-			}
+			
 			elseif($textin_cmd[0]=='สถานะ')
 			{
 				$FIREBASE = "https://esp8266-temp.firebaseio.com/";
