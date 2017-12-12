4e059bff7a0fe58a5f06727ddb54926978cd5bad
Seksan <microsekmcu@gmail.com>
2017-12-12 09:50:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,19 +1,4 @@
 <?php
 	echo "verify\n";
-	$Node=$_GET["node"];
-	$cmd=(int)$_GET['cmd'];
-        $FIREBASE = "https://esp8266-temp.firebaseio.com/";
-	$NODE_PATCH = "Lamp.json";
-	$data = array(
-    		$Node => $cmd
-	);
-	$json = json_encode( $data );
-	$curl = curl_init();
-	curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
-	curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
-	curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
-	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-	$response = curl_exec( $curl );
-	curl_close( $curl );
-	echo $response . "\n";
+	
 ?>
